b3ea1bee60b66b30cbb2b5e4bf8e5015305fa787
QueenVol <159581003+QueenVol@users.noreply.github.com>
2025-09-14 17:43:55 -0400

    Adding some Audio

--- Water Bottle vs. Dishwasher/Assets/Script/Bottle/Bottle.cs
+++ Water Bottle vs. Dishwasher/Assets/Script/Bottle/Bottle.cs
@@ -10,17 +10,25 @@ public class Bottle : MonoBehaviour
     [SerializeField] private Sprite brokenBottle;
     [SerializeField] private GameObject endingScreen;
     [SerializeField] private GameObject processSlider;
+    [SerializeField] private GameObject up;
+    [SerializeField] private GameObject down;
+    [SerializeField] private GameObject left;
+    [SerializeField] private GameObject right;
+    [SerializeField] private AudioSource backgroundMusic;
+    [SerializeField] private AudioSource backgroundNoise;
+    [SerializeField] private AudioSource endMusic;
     private SpriteRenderer spriteRenderer;
     private bool isBroken = false;
 
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        backgroundMusic.PlayDelayed(3f);
     }
 
     private void Update()
     {
-        Debug.Log(breakProgress);
+        //Debug.Log(breakProgress);
         if(breakProgress > 1f )
         {
             breakProgress = 1f;
@@ -43,6 +51,9 @@ public class Bottle : MonoBehaviour
         {
             endingScreen.SetActive(true);
             processSlider.SetActive(false);
+            backgroundMusic.Stop();
+            backgroundNoise.Stop();
+            endMusic.Play();
         }
 
         Time.timeScale = 0f;
